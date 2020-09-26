89368879c18cef1f93a94bb46254a95e38077195
k45734 <71308262+k45734@users.noreply.github.com>
2020-09-27 06:39:09 +0900

    Update logic_ani365.py

--- logic_ani365.py
+++ logic_ani365.py
@@ -214,12 +214,12 @@ class Ani365QueueEntity(FfmpegQueueEntity):
 
     def make_episode_info(self):
         try:
-			test = 'https://www.ani365.me/kr/detail/' + self.info['content_code']
-			s = requests.session()
-			s.get(url,headers=headers, verify=False)
-			url = 'https://www.jetcloud-list.cc/kr/episode/' + self.info['va']
-			text = requests.get(url, headers=headers).content
-			match = re.compile('src\=\"(?P<video_url>http.*?\.m3u8)').search(text)
+            test = 'https://www.ani365.me/kr/detail/' + self.info['content_code']
+            s = requests.session()
+            s.get(url,headers=headers, verify=False)
+            url = 'https://www.ani365.me/kr/episode/' + self.info['va']
+            text = requests.get(url, headers=headers).content
+            match = re.compile('src\=\"(?P<video_url>http.*?\.m3u8)').search(text)
             if match:
                 tmp = match.group('video_url')
                 m3u8 = requests.get(tmp).content
