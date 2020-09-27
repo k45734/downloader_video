897f7da4604ee113361c7e48dc59b8e8ac3b7dc4
k45734 <71308262+k45734@users.noreply.github.com>
2020-09-27 11:00:57 +0900

    Update logic_ani365.py

--- logic_ani365.py
+++ logic_ani365.py
@@ -260,7 +260,7 @@ class Ani365QueueEntity(FfmpegQueueEntity):
             from framework.common.util import write_file, convert_vtt_to_srt
             srt_filepath = os.path.join(self.savepath, self.filename.replace('.mp4', '.ko.srt'))
             if not os.path.exists(srt_filepath):
-                vtt_data = requests.get(self.vtt).content
+                vtt_data = requests.get(self.vtt).text
                 srt_data = convert_vtt_to_srt(vtt_data)
                 write_file(srt_data, srt_filepath)
         except Exception as e:
