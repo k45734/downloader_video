46f14e223009c9d914ad4e14092ea4a7b68a24f1
k45734 <71308262+k45734@users.noreply.github.com>
2020-10-15 17:22:31 +0900

    Update logic_ani365.py

--- logic_ani365.py
+++ logic_ani365.py
@@ -235,7 +235,10 @@ class Ani365QueueEntity(FfmpegQueueEntity):
                         self.quality = t.split('.m3u8')[0]
             match = re.compile('src\=\"(?P<vtt_url>http.*?\kr.vtt)').search(text)
             if match:
-                self.vtt = u'%s' % match.group('vtt_url')
+                   try:
+                        self.vtt = u'%s' % match.group('vtt_url')
+                   execpt:
+                        self.vtt = match.group('vtt_url')
             match = re.compile(ur'(?P<title>.*?)\s*((?P<season>\d+)기)?\s*((?P<epi_no>\d+)화)').search(self.info['title'])
             if match:
                 self.content_title = match.group('title').strip()
@@ -272,7 +275,7 @@ class Ani365QueueEntity(FfmpegQueueEntity):
                     srt_data = convert_vtt_to_srt(vtt_data)
                     write_file(srt_data, srt_filepath)
             except:
-                vtt_data = requests.get(self.vtt, headers=request_headers)
+                vtt_data = requests.get(self.vtt, headers=request_headers).content
                 write_file(vtt_data, srt_filepath2)
                 srt_data = convert_vtt_to_srt(vtt_data)
                 write_file(srt_data, srt_filepath)
